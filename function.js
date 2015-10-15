4585543ffecbc5d050ddb1c52181703d055467bb
torwood3 <pierre.aymeric.masse@gmail.com>
2015-10-15 10:59:41 +0200

    add jquery

--- function.js
+++ function.js
@@ -1,43 +1,42 @@
-// Variable globale
-var counter = 0;
 
-function addTodo() {
-  // Récupération du message
-  var input = document.getElementById("todoMessage");
-  var todoMessage = input.value;
-  // On efface le champs liste
-  input.value = "";
-  // On récupère l'élément liste
-  var listContainer = document.getElementById("todoList");
-  // Création du nouvel élément de la liste
-  var newElement = document.createElement("li");
-  var newElementId = counter++;
-  newElement.setAttribute("id", newElementId);
-  newElement.innerHTML = "<input type='checkbox' name='todoCheck' value='" + newElementId + "'> " + todoMessage;
-  // Ajout du nouvel élément à la liste
-  listContainer.appendChild(newElement);
-}
+//On attend la fin du chargement de kla page
+$().ready(function(){
+  // Variable globale
+  var counter = 0;
 
-function removeAll() {
-  // On récupère l'élément liste
-  var listContainer = document.getElementById("todoList");
-  // On vide son contenu
-  listContainer.innerHTML = "";
-}
+  //Binding des evenements et des fonctions
+  $("input[value='Ajouter']").click(addTodo);
+  $("input[value='Tout effacer']").click(removeAll);
+  $("input[value='Supprimer la séléction']").click(removeSel);
 
-function removeSel() {
-  // On récupère les éléments du DOM
-  var listeContainer = document.getElementById("todoList");
-  var checkboxes = document.getElementsByName("todoCheck");
-  // On boucle sur chaque élément
-  for(var i = checkboxes.length-1; i >= 0; i--) {
-    // Si la checkbox est cochée ...
-    if(checkboxes[i].checked) {
-      // On supprime la ligne !
-      var todo = document.getElementById(checkboxes[i].value);
-      todo.parentNode.removeChild(todo);
-    }
+
+  function addTodo() {
+    // Récupération du message
+    var todoMessage = $("#todoMessage").val();
+    $("#todoMessage").val(""); //On vide le contenu de l'input
+
+    // Création du nouvel élément de la liste
+    //Maintenant, on se base QUE sur le faite que l'element et coché pour le récupéré (voir removeSel())
+    var newElement = "<li><input type='checkbox'> "+ todoMessage + "</li>";
+    counter++;
+    // Ajout du nouvel élément à la liste
+    $("#todoList").append(newElement);
   }
-}
 
+  function removeAll() {
+    // On récupère l'élément liste et On vide son contenu
+    $("#todoList").children().remove();
+  }
 
+  function removeSel() {
+    // On récupère les éléments coché. Il sont fourni sous forme d'un tableau
+    var checkboxes = $("input:checked");// input:checked => tous les inputs qui sont cochés
+
+    // each va parcourir l'ensemble du tableau et fournir les index de chaque element.
+    // cela evite de faire for (i=0; i < tab.lenght -1; i++)
+    checkboxes.each(function(index){
+      // On supprime le parent (<li>) de l'element correspondant du tableau
+      checkboxes[index].parentNode.remove();
+    });
+  }
+});
